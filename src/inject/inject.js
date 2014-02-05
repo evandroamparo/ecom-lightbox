b49003b1f197340ed03158612ce8ea638224c9cc
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2014-02-05 10:27:38 -0200

    Biblioteca fancyBox

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,10 +3,14 @@ function init() {
     var $links = $("a[href^='download.php'][target='_blank']").filter(function(index) {
       return ($(this).text().match(/.*jpg|png|gif|bmp/i) != null)
     });
-    $links.each(function(i, val) {
-        $(val).attr('data-lightbox', 'ecom');
-        $(val).attr('title', $(val).text());
-    });
+    $links
+      .attr('rel', 'ecom-gallery')
+      .fancybox({
+        type: "image",
+        beforeLoad: function() {
+            this.title = $(this.element).text();
+        }
+      });
   }
 }
 
